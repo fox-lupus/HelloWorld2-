970b10cf8b624a86ed7bff618906c37a6e242bf4
milo <42156458+fox-lupus@users.noreply.github.com>
2025-02-07 17:07:42 -0500

    added another hi bob

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ class HelloWorld {
         System.out.println("Hello, World!"); 
         System.out.println("hi, Alice"); 
         System.out.println("hi, Bob"); 
+        System.out.println("hi, Bob"); 
     }
 }
