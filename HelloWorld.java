1f1a5e793f4d07705553a157c3ebf546b3f83e9b
milo <42156458+fox-lupus@users.noreply.github.com>
2025-02-12 16:15:38 -0500

    Closes printDate and printFreememory issues

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,11 @@
 import java.util.Date;
 
 class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-
-        System.out.println(new Date().toString()); 
+    public static void main(String[] args) { 
+        System.out.println("Hello, World!"); 
+        //run time holds the memory
         Runtime runtime = Runtime.getRuntime();
         System.out.printf("free memory for JVM %d (MB) \n", runtime.freeMemory() / (1024 * 1024) ); 
-
         System.out.println(new Date().toString()); 
 
         System.out.println("hi, Alice"); 
